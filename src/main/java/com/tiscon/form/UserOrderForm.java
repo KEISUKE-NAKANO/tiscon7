a996b968d65a9bc3fa87576f35b215a7a496943a
nakanokeisuke <nakano.keisuke0512@gmail.com>
2022-01-29 17:09:18 +0900

    Changes nput scheduleddateの追加 result　scheduleddateの追加 UserOrderForm　scheduleddateの追加

--- src/main/java/com/tiscon/form/UserOrderForm.java
+++ src/main/java/com/tiscon/form/UserOrderForm.java
@@ -38,6 +38,10 @@ public class UserOrderForm {
 
     @Numeric
     @NotBlank
+    private String scheduleddate;
+
+    @Numeric
+    @NotBlank
     private String box;
 
     @Numeric
@@ -109,6 +113,12 @@ public class UserOrderForm {
 
     public void setNewAddress(String newAddress) {
         this.newAddress = newAddress;
+    }
+
+    public String getScheduleddate() { return scheduleddate; }
+
+    public void setScheduleddate(String scheduleddate) {
+        this.scheduleddate = scheduleddate;
     }
 
     public String getBox() {
